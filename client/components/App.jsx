d1c6a83b8f6e747052ee95857fcad69476e5d888
maddyking44 <madelineking44@gmail.com>
2018-04-29 10:35:27 +1200

    beers boilerplate

---
+++ client/components/App.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+// This might need to be turned into a stateful (class-based) component
+const App = () => (
+  <div className='app'>
+    Ready to rock and roll
+  </div>
+)
+
+export default App
